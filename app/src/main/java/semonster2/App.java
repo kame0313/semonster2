8be5f60cdad641c4e593cfd77c13c79cfc8f35ad
Yuya <e1c20056@oit.ac.jp>
2023-07-03 10:45:04 +0900

    動作確認済

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        Monster monster = new Monster(1, 1);
+        Monster monster = new Monster(2, 2);
         System.out.print(monster.toString());
     }
 }
