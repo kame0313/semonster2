1177b816ac54fe980399fb32268e75f33f0065f2
kakuho <m1m23a07@oit.ac.jp>
2023-07-03 10:26:27 +0900

    コンパイルできた

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        Monster monster = new Monster(0, 0);
+        Monster monster = new Monster(1, 0);
         System.out.print(monster.toString());
     }
 }
